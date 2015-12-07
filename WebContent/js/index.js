c7a897b46a9e1771305be236acd65e25097bda89
Nikhil Ambekar <nikhil.a.ambekar@gmail.com>
2015-12-07 03:32:37 -0500

    Adding some screens

---
+++ WebContent/js/index.js
@@ -0,0 +1,77 @@
+/**
+ * Created by Nikhil on 11/12/2015.
+ */
+$(document)
+    .ready(
+        function () {
+            $('[data-toggle=offcanvas]').click(function() {
+                $('.row-offcanvas').toggleClass('active');
+            });
+            var i = 1;
+            $("#add_row")
+                .click(
+                    function() {
+                        $('#addr' + i)
+                            .html(
+                                "<td>"
+                                + (i + 1)
+                                + "</td><td><input name='dosageName"
+                                + i
+                                + "' type='text' placeholder='Name and Dosage' class='form-control input-md'  /></td><td><input  name='time"
+                                + i
+                                + "' type='text' placeholder='Time'  class='form-control input-md'></td><td><input  name='route"
+                                + i
+                                + "' type='text' placeholder='Route'  class='form-control input-md'></td><td><input  name='site"
+                                + i
+                                + "' type='text' placeholder='Site'  class='form-control input-md'></td><td><input  name='md"
+                                + i
+                                + "' type='text' placeholder='MD'  class='form-control input-md'></td><td><input  name='rnintials"
+                                + i
+                                + "' type='text' placeholder='RN Initials'  class='form-control input-md'></td>");
+
+                        $('#tab_logic').append(
+                            '<tr id="addr' + (i + 1)
+                            + '"></tr>');
+                        i++;
+                    });
+            $("#delete_row").click(function() {
+                if (i > 1) {
+                    $("#addr" + (i - 1)).html('');
+                    i--;
+                }
+            });
+
+
+
+            $('#displayName').text(sessionStorage.getItem("name"));
+            $('#displayDob').text(sessionStorage.getItem("dob"));
+            var htmlToAdd = "";
+            var gen = sessionStorage.getItem("gender");
+            if(gen=="male"){
+                htmlToAdd =  "<i class='fa fa-male fa-lg'></i> Male";
+            }else{
+                htmlToAdd =  "<i class='fa fa-female fa-lg'></i> Female";
+            }
+            $('#displayGender').html(htmlToAdd);
+            $('#displayWeight').text(sessionStorage.getItem("weight")+" Kgs.");
+            var level = sessionStorage.getItem("emergencyLevel");
+            if(level == "Level 1"){
+                $('#displayLevel').addClass("label label-danger");
+            }else if(level == "Level 2"){
+                $('#displayLevel').addClass("label label-warning");
+            }else{
+                $('#displayLevel').addClass("label label-info");
+            }
+            $('#displayLevel').text(level);
+            tabs = $('ul.tabNavigate li a');
+
+            tabs.bind('click',function(event){
+                var $anchor = $(this);
+                $("#primSurvey").hide();
+                $("#secondarySurvey").hide();
+                $("#medications").hide();
+                event.preventDefault();
+                $($anchor.attr('href')).show();
+                event.preventDefault();
+            });
+        });
---
+++ WebContent/js/main.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Nikhil on 11/11/2015.
+ */
+
+$(document)
+    .ready(
+        function () {
+            $('#arrivalTimeDatePicker')
+                .datetimepicker({
+                    sideBySide: true
+                });
+            $('#dobDatePicker')
+                .datetimepicker({
+                    format: 'MM/DD/YYYY'
+                });
+            $("#arrivalMode").imagepicker();
+            $("#create").on('click', function(event) {
+                sessionStorage.setItem("name",$("#name").val());
+                sessionStorage.setItem("dob",$("#dob").val());
+                sessionStorage.setItem("gender",$("#gender input:radio:checked").val());
+                sessionStorage.setItem("arrivalTime",$("#arrivalTime").val());
+                sessionStorage.setItem("arrivalMode",$("#arrivalMode").val());
+                sessionStorage.setItem("weight",$("#weight").val());
+                sessionStorage.setItem("emergencyLevel",$("#emergenceyLevel input:radio:checked").val());
+                event.preventDefault();
+                $('#newEMREntry').modal('toggle');
+                $('#redirect').click();
+                window.location.href = "index.html";
+            });
+        });
